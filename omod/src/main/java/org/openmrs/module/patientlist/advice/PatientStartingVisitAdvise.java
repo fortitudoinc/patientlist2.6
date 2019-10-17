7d2ff291404974bf6c75be4c8abc8c74cb22b433
Barry Levine <levine12@gmail.com>
2019-10-17 10:25:31 -0700

    allow only one dr. to call patient

--- omod/src/main/java/org/openmrs/module/patientlist/advice/PatientStartingVisitAdvise.java
+++ omod/src/main/java/org/openmrs/module/patientlist/advice/PatientStartingVisitAdvise.java
@@ -8,34 +8,37 @@ package org.openmrs.module.patientlist.advice;
 import java.lang.reflect.Method;
 import java.util.Date;
 import java.util.List;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.openmrs.Patient;
 import org.openmrs.User;
 import org.openmrs.Visit;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.patientlist.PatientListItem;
 import org.openmrs.module.patientlist.api.PatientListItemService;
-import org.openmrs.module.uicommons.util.InfoErrorMessageUtil;
-import org.springframework.aop.AfterReturningAdvice;
+import org.springframework.aop.MethodBeforeAdvice;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * @author levine
  */
-public class PatientAfterEndVisitAdvice implements AfterReturningAdvice {
+public class PatientStartingVisitAdvise implements MethodBeforeAdvice {
 	
 	private Log log = LogFactory.getLog(this.getClass());
 	
-	public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
-		if (!method.getName().equals("endVisit")) {
+	@Override
+	public void before(Method method, Object[] args, Object o) throws Throwable {
+		if (!method.getName().equals("saveVisit")) {
 			return;
 		}
+		o.getClass().getName();
+		System.out.println("o.getClass().getName(): " + o.getClass().getName() + "\nargs[0].getName(): "
+		        + args[0].getClass().getName());
 		Visit visit = (Visit) args[0];
 		Patient patient = visit.getPatient();
 		User user = Context.getAuthenticatedUser();
-		System.out.println("*********ENDING VISIT REMOVE FROM LIST: " + patient.getFamilyName() + " Patient Id: "
+		System.out.println("*********Starting VISIT REMOVE FROM LIST: " + patient.getFamilyName() + " Patient Id: "
 		        + patient.getPatientId());
-		log.debug("Method: " + method.getName() + ". After advice called ");
+		log.debug("Method: " + method.getName() + ". Before advice called ");
 		
 		int patientId = patient.getPatientId();
 		PatientListItem patientListItem = null;
@@ -43,15 +46,16 @@ public class PatientAfterEndVisitAdvice implements AfterReturningAdvice {
 		List<PatientListItem> items = Context.getService(PatientListItemService.class).getActivePatientListItemForPatient(
 		    patientId);
 		if (items.size() != 1) {
-			System.out.println("AFTER RETURNING - NO PATIENT MATCH FOR ID: " + patientId);
+			System.out.println("BEFORE RETURNING - NO ACTIVE PATIENT MATCH FOR ID: " + patientId);
 			return;
 		}
 		patientListItem = items.get(0);
 		
-		System.out.println("*********ENDING VISIT REMOVE FROM LIST item id: " + patientListItem.getId());
+		System.out.println("*********Starting VISIT REMOVE FROM LIST item id: " + patientListItem.getId());
 		patientListItem.setLastContactAttemptDate(new Date());
 		patientListItem.setHasBeenCalled(1);
 		patientListItem.setDrPersonId(user.getPerson().getPersonId());
 		Context.getService(PatientListItemService.class).savePatientListItem(patientListItem);
 	}
+	
 }
